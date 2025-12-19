5bf9637504cffb38557d227ec4a9add1ddee8324
Jenny Alvarez <jennyalvarezzg@gmail.com>
2025-12-19 13:46:15 +0100

    uncomment line

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ app.get("/api/project/:projectId", async (req, res) => {
 
   console.log('Los datos que obtenemos de la BBDD', data)
   // Respondemos con la pagina de detalles mandandole los datos que hemos obtenido
-  //res.render("details", data);
+  res.render("details", data);
   // Cerramos la conexion
   connection.end();
 });
